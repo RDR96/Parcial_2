03a354668e4bfa0a14daafa376b720414845986b
RDR96 <00231514@uca.edu.sv>
2018-06-02 00:20:48 -0600

    Se añadieron elemementos al navigation view, se agrego funcion para colocar imagenes de forma aleatoria al encabezado del drawer

--- app/src/main/java/com/rdr/rodrigocorvera/gamenews/MainActivity.java
+++ app/src/main/java/com/rdr/rodrigocorvera/gamenews/MainActivity.java
@@ -1,14 +1,23 @@
 package com.rdr.rodrigocorvera.gamenews;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.support.annotation.NonNull;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
@@ -20,31 +29,66 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     Toolbar toolbar;
 
+    ImageView imageView;
+
+    Resources res;
+
+    TypedArray images;
+
+    Random random;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(toolbar);
-
         getViews();
+        setConfiguration();
 
     }
 
-    public void getViews() {
-        mNavigationView= findViewById(R.id.navigation_view);
-        drawerLayout = findViewById(R.id.drawer_layout);
-        toogle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+    public void setConfiguration () {
+        random = new Random();
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        res = getResources();
+        images = res.obtainTypedArray(R.array.header_images);
+        toogle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close){
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                imageView.setImageDrawable(images.getDrawable(getRandomValue(images.length())));
+
+            }
+
+        };
+
         drawerLayout.addDrawerListener(toogle);
         toogle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        mNavigationView = findViewById(R.id.navigation_view);
         if (mNavigationView != null) {
             mNavigationView.setNavigationItemSelectedListener(this);
         }
     }
 
+    public void getViews() {
+        mNavigationView= findViewById(R.id.navigation_view);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        imageView = mNavigationView.getHeaderView(0).findViewById(R.id.header_image);
+        mNavigationView = findViewById(R.id.navigation_view);
+    }
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -53,15 +97,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
-
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        drawerLayout.closeDrawers();
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -73,5 +108,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return super.onOptionsItemSelected(item);
     }
 
+    public int getRandomValue (int n) {
+        return random.nextInt(n);
+    }
 
 }
+
+
+
+
+
