16d6f1970e3fb9d51a2e0b74788777fc7b450013
RDR96 <00231514@uca.edu.sv>
2018-06-01 19:22:35 -0600

    Se agrego un toolbar, se puso el navigation view con la funcionalidad de abrir tocando boton y deslizando el dedo, se agrego boton de buscar

--- app/src/main/java/com/rdr/rodrigocorvera/gamenews/MainActivity.java
+++ app/src/main/java/com/rdr/rodrigocorvera/gamenews/MainActivity.java
@@ -1,14 +1,77 @@
 package com.rdr.rodrigocorvera.gamenews;
 
+import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+
+    ActionBarDrawerToggle toogle;
+
+    NavigationView mNavigationView;
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        getViews();
+
     }
+
+    public void getViews() {
+        mNavigationView= findViewById(R.id.navigation_view);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toogle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toogle);
+        toogle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mNavigationView = findViewById(R.id.navigation_view);
+        if (mNavigationView != null) {
+            mNavigationView.setNavigationItemSelectedListener(this);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_toolbar_items, menu);
+        getSupportActionBar().setTitle(R.string.app_name);
+        return true;
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        drawerLayout.closeDrawers();
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //getSupportActionBar().show();
+        if (toogle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
