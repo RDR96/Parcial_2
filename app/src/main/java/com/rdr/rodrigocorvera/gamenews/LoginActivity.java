8b6f20033e73520ff959bfaab68c3cd495e5e5a2
RDR96 <00231514@uca.edu.sv>
2018-06-03 19:45:30 -0600

    Se termino la parte visual del inicio de sesion, el xml, agrego el color principal y el secundario (por el momento), y algunos textos como recurso

--- app/src/main/java/com/rdr/rodrigocorvera/gamenews/LoginActivity.java
+++ app/src/main/java/com/rdr/rodrigocorvera/gamenews/LoginActivity.java
@@ -1,13 +1,58 @@
 package com.rdr.rodrigocorvera.gamenews;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView textButtonToRegister;
+    Button buttonSignIn;
+    EditText textFieldName;
+    EditText textFieldPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getViews();
+        setConfiguration();
+
+
     }
+
+    public void getViews () {
+        textButtonToRegister = findViewById(R.id.text_to_register);
+        buttonSignIn = findViewById(R.id.button_sign_in);
+        textFieldName = findViewById(R.id.textfield_name);
+        textFieldPassword = findViewById(R.id.textfield_name);
+    }
+
+    public void setConfiguration () {
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( !textFieldName.getText().equals("") && !textFieldPassword.getText().equals("") ) {
+
+                } else {
+                    Toast.makeText(LoginActivity.this, R.string.fill_fields, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        textButtonToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 }
